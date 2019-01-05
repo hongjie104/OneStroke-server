746f88b21cf57371db421d4d64aa779ddf17da82
hongjie <32968210@qq.com>
2019-01-05 17:04:39 +0800

    test

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require('../core/baseController');
 class HomeController extends Controller {
     async index() {
         // this.success('hi, welcome to One-Stroke-Server!');
+        this.ctx.logger.error(JSON.stringify(this.ctx.query));
         this.success(this.ctx.query);
     }
 
