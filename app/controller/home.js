9127bf52fe0c8a283c797ca6deff7e6223737e99
hongjie <32968210@qq.com>
2019-01-05 17:08:59 +0800

    test

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,9 +4,18 @@ const Controller = require('../core/baseController');
 
 class HomeController extends Controller {
     async index() {
-        // this.success('hi, welcome to One-Stroke-Server!');
-        this.ctx.logger.error(JSON.stringify(this.ctx.query));
-        this.success(this.ctx.query);
+        // {
+        // "signature":"b5682c4d92c5d05c3e6ef085b9002d3b24396561",
+        // "echostr":"331186382028045302",
+        // "timestamp":"1546679138",
+        // "nonce":"649492913"
+        // }
+        // 1）将token、timestamp、nonce三个参数进行字典序排序
+        // 2）将三个参数字符串拼接成一个字符串进行sha1加密
+        // 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+        // this.success(this.ctx.query);
+        const { echostr } = this.ctx.query;
+        this.ctx.body = echostr;
     }
 
     async test() {
